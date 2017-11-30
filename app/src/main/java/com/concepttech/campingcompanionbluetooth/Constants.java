560f74294832fc63fec3cc94f5718187367692d3
Giang Ly <gkly@shockers.wichita.edu>
2017-11-30 11:10:55 -0600

    First Commit

---
+++ app/src/main/java/com/concepttech/campingcompanionbluetooth/Constants.java
@@ -0,0 +1,99 @@
+package com.concepttech.campingcompanionbluetooth;
+
+/**
+ * Created by jtint on 11/15/2017.
+ */
+
+public class Constants {
+    public static final String MTYPE = "messageType",
+            MARGS = "numArgs",
+            ASIZE = "arraySize",
+            VALUES = "values",
+            ROWS = "Rows",
+            COLUMNS = "Columns",
+            DATASEP = "|",
+            LABELDATASEP = ":",
+            DATAEND = ";",
+            BODYBEGIN = "<",
+            BODYEND = "/>",
+            INTNUMERICREGEX = "[0-9]+",
+            DOUBLENUMERICREGEX = "[-+]?[0-9]*\\.?[0-9]+",
+            RESENDLOGMESS = "Resend Requested",
+            ERR = "Error: ",
+            MESS = "Message: ",
+            RTRERR = "RTR mismatch",
+            MESSAGENULLERR = "Message was null",
+            MESSAGEEMPTYERR = "Message was empty",
+            TYPEARGERR = "Message did not contain either type or numargs",
+            MESSAGETYPEEMPTY = "Message type was empty",
+            MESSAGENUMARGSEMPTY = "Message numArgs was empty",
+            MESSAGETYPENOTNUMORLONG = "Message type was not numeric or to long",
+            MESSAGEARGSNOTNUMORLONG = "Message numArgs was not numeric or to long",
+            MESSAGETYPE = "Message type parsed messagetype: ",
+            NOCOLERR = "Message did not contain any colons",
+            MESSTOPOSTEMPTY = "Message to post to log was length 0, no resend requested",
+            MESSTOPOSTNULL = "Message to post to log was null, no resend requested",
+            FALSE = "false",
+            TRUE = "true",
+            DEVICE_NAME = "device_name",
+            TOAST = "toast",
+            LIGHTSTATUS = "LIGHTSTATUS",
+            LIGHTCOLORS ="LIGHTCOLORS";
+
+    public static final int MESSAGE_STATE_CHANGE = 1,
+            MAXCOLORCODES = 3,
+            MAXCOLORCODEVALUE = 256,
+            MESSAGE_READ = 2,
+            MESSAGE_WRITE = 3,
+            MESSAGE_DEVICE_NAME = 4,
+            MESSAGE_TOAST = 5,
+            INIT = 0,
+            UPADATEDATAPHONE2HUB = 1,
+            UPDATEDATAHUB2PHONE = 2,
+            COMMAND = 3,
+            SOCIALMESSAGE = 4,
+            COMMANDCONFIRMATION = 5,
+            RESENDREQUEST = 6,
+            RESENDREQUESTCONFIRMATION = 7,
+            GENERALCONFIRMATION = 9,
+            MAXALLOWEDTYPEDIGITS = 1,
+            INITEXPECTEDARGS = 5,
+            MESSAGETYPERETURNERROR = -1,
+            MESSAGETYPEMAXVALUE = 9;
+    public static final String[] LIGHTCOMMANDLABELS = {LIGHTSTATUS,LIGHTCOLORS};
+    public static final String[] LABELS = {
+        "TEMP",	"HUMIDITY", "BAROPRESS", "BATTERYLEVEL", "LIGHTSTATUS",
+                "NUMLIGHTS", "LIGHTNAMES", "LIGHTCOLORS", "SOLAR","WIND",
+                "LATITUDE", "LONGITUDE", "BROADCAST", "HUBNAME", "NETWORKED",
+                "NUMNODES", "NODENAMES", "ALLOWSCONTROL", "ALLOWSCHAT", "SLAVE", "OTHERLIGHTNAMES",
+                "OTHERNUMLIGHTS", "OTHERLIGHTSTATUS", "CONNECTIONERROR", "MESSAGEDEST", "MESSAGESOURCE",
+                "REASON"
+    };
+    public final int LabelIndex(String label){
+        if(label != null){
+            if(label.length() > 0){
+                int i;
+                for (i = 0; i < LABELS.length; i++){
+                    if(label.equals(LABELS[i])){
+                        return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    public final boolean isValidLabel(String labelcandidate){
+        if(labelcandidate != null){
+            if(labelcandidate.length() > 0){
+                int i;
+                for (i = 0; i < LABELS.length; i++){
+                    if(labelcandidate.equals(LABELS[i])){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/concepttech/campingcompanionbluetooth/Main.java
@@ -0,0 +1,31 @@
+package com.concepttech.campingcompanionbluetooth;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class Main extends FragmentActivity implements BluetoothController.OnFragmentInteractionListener{
+
+    public void onFragmentInteraction(Uri uri){
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothController fragment = new BluetoothController();
+            transaction.replace(R.id.fragment_holder, fragment);
+            transaction.commit();
+        }
+    }
+}
