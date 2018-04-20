6ca92b0da9227912599a7f11bc2356c5820b3ca6
Giang Ly <gkly@shockers.wichita.edu>
2018-04-20 14:13:14 -0500

    UI Updated
    UI Push

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -8,17 +8,17 @@ package com.google.android.gms;
 
 public final class R {
     public static final class id {
-        public static final int none = 0x7f07005b;
-        public static final int normal = 0x7f07005c;
-        public static final int radio = 0x7f070067;
-        public static final int text = 0x7f07008e;
-        public static final int text2 = 0x7f07008f;
-        public static final int wrap_content = 0x7f07009f;
+        public static final int none = 0x7f07005c;
+        public static final int normal = 0x7f07005d;
+        public static final int radio = 0x7f070068;
+        public static final int text = 0x7f07008f;
+        public static final int text2 = 0x7f070090;
+        public static final int wrap_content = 0x7f0700a0;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f080004;
     }
     public static final class string {
-        public static final int common_google_play_services_unknown_issue = 0x7f0b0027;
+        public static final int common_google_play_services_unknown_issue = 0x7f0b0029;
     }
 }
